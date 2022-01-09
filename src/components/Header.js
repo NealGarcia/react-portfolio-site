ba468fbbdaebad5c6eeb1db2efa4378b7c209b30
nealgarcia <nathnlgarcia@gmail.com>
2022-01-08 23:20:00 -0600

    full navbar functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,21 +6,24 @@ import "react-toggle/style.css"
 
 function Header(props) {
 
-    function scrollTo(){
+    function scrollToProjects(){
         scroll.scrollTo(300);
+    }
+
+    function scrollToAbout(){
+        scroll.scrollToBottom({behavior:"smooth"});
     }
     return (
         <div>
             <DarkMode />
             <div className = "titleWrapper">
-                {/* <h2 className = "name">Neal Garcia</h2> */}
                 <h1 className = "titleHeader">
                     I'm Neal Garcia, a software engineer + web developer <span>currently</span> living in <span>Dallas, Texas.</span>
                 </h1>
                 <ul className = "nav">
-                    <li onClick = {scrollTo}><button>Projects</button>
+                    <li onClick = {scrollToProjects}><button>Projects</button></li>
+                    <li onClick = {scrollToAbout}><button>About</button>
                     </li>
-                    <li>About</li>
                     <li><a href = "https://www.linkedin.com/in/nealgarcia" target="_blank" rel="noreferrer">LinkedIn</a></li>
                     <li><a href = "https://github.com/NealGarcia" target="_blank" rel="noreferrer">Github</a></li>
                     <li><a href={resume} target='_blank' rel='noopener noreferrer'>Resume</a></li>
