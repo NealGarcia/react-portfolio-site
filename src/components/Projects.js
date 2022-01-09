5291db252ddec7966cc55e9647d5b8e4d19e7e83
nealgarcia <nathnlgarcia@gmail.com>
2022-01-09 15:48:51 -0600

    added links to individual project repos and sites

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,6 +9,8 @@ const projects = [
         description:"An outer-space themed browser game made with HTML, CSS, and vanilla JavaScript, where I explored DOM Manipulation methods.",
         tags: "JavaScript, HTML, CSS, Front End Development, Web Design",
         img: screen1,
+        repo: "https://pages.git.generalassemb.ly/nealgarcia/spaceman/",
+        site: "https://github.com/NealGarcia/Spaceman"
     },
     {
         id: 2,
@@ -16,6 +18,8 @@ const projects = [
         description:"A front-end web application that utilizes third-party data from the Art Institute of Chicago API to allow users to search, view, and learn more about artworks in their collections.",
         tags: "Front End Development, Web Design, ReactJS, API",
         img: screen2,
+        repo: "https://github.com/NealGarcia/Art-Gallery",
+        site: "https://proj-2-react-art-gallery.herokuapp.com/",
     }
 ]
 
@@ -24,11 +28,15 @@ function Projects(props) {
         <div className = "projectsContainer">
             {projects.map(project => (
                 <div key = {project.id} className = "projectCard">
-                    <img src = {project.img} alt = "Project Image" />
+                    <img src = {project.img} alt = "Project" />
                     <div className = "projectTextWrapper">
                         <h2 className = "projectTitle">{project.name}</h2>
                         <p className = "projectTags">{project.tags}</p>
                         <p className = "projectDescription">{project.description}</p>
+                        <div className = "projectLinks">
+                            <p><a href = {project.repo} target="_blank" rel="noreferrer">View Source Code</a> →</p>
+                            <p><a href = {project.site} target="_blank" rel="noreferrer">View Live Project </a>→</p>
+                        </div>
                     </div>
                 </div>
             ))}
