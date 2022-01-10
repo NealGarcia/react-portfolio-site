0ffe1ab88e90c670379e83fae8ba4dd99b5c5cdc
nealgarcia <nathnlgarcia@gmail.com>
2022-01-09 21:21:50 -0600

    deployed to github pages

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ function About(props) {
             <h1 className = "aboutTitle">About Me</h1>
             <div className = "aboutMe">
                 <img src = {avatar} alt = "Me" className = "aboutImg"/>
-                <p>Hi, 👋  I'm <span>Neal</span> — I was born a Chicagoan, raised an Austinite, and am currently a Dallas native. 
+                <p>Hi, 👋  I'm <span>Neal</span> —  I'm a software engineer, web developer, & tech enthusiast currently based in Dallas, TX. 
                 I graduated from the University of Texas at Dallas with a degree in Emerging Media and Communication and am 
                 a soon to be graduate of General Assembly's Software Engineering Immersive Program with an expected completion date of late January 2022.</p>
                 <p>When I am not coding, you can find me cycling around White Rock Lake here in Dallas, going for a run, or playing with my dog <a href = "https://www.instagram.com/spaceysagey/">Sage</a>.</p>
