d3264f43cda8e5bd58bc2b6143e8d7419b278cda
neal-garcia95 <neal@builtbymasonry.com>
2023-01-07 10:57:51 -0600

    topNav styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,8 @@ import resume from '../assets/resume.pdf'
 import DarkMode from './DarkMode';
 import { Link, animateScroll as scroll } from "react-scroll";
 import { ReactComponent as Pin } from '../assets/pin.svg'
-import "react-toggle/style.css" 
+import "react-toggle/style.css"
+import "../styles/nav.css" 
 
 function Nav(props) {
 
@@ -15,9 +16,11 @@ function Nav(props) {
         scroll.scrollTo(1300);
     }
     return (
-        <div>
+        <div className="topNavWrapper">
+            <div className="topNav">
             <DarkMode />
-
+            <p className="aboutNav">About</p>
+        </div>
         </div>
     );
 }
