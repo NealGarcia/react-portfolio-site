3f445cf4530546449a6892bf24286364afd4da50
neal-garcia95 <neal@builtbymasonry.com>
2023-01-07 10:28:15 -0600

    update github link

--- src/components/Header.js
+++ src/components/Header.js
@@ -26,7 +26,7 @@ function Header(props) {
                     <li onClick = {scrollToAbout}><button>About</button>
                     </li>
                     <li><a href = "https://www.linkedin.com/in/nealgarcia" target="_blank" rel="noreferrer">LinkedIn</a></li>
-                    <li><a href = "https://github.com/NealGarcia" target="_blank" rel="noreferrer">Github</a></li>
+                    <li><a href = "https://github.com/neal-garcia95" target="_blank" rel="noreferrer">Github</a></li>
                     <li><a href={resume} target='_blank' rel='noopener noreferrer'>Resume</a></li>
                     <li><a href = "mailto: nathnlgarcia@gmail.com" >Contact</a></li>
                 </ul>
