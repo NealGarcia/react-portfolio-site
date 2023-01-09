04ace87bfeb13654776ad34fd14510a5e280dd55
neal-garcia95 <neal@builtbymasonry.com>
2023-01-08 19:41:07 -0600

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,18 +4,18 @@ import { ReactComponent as IgIcon } from '../assets/SVGs/ig.svg'
 import { ReactComponent as LinkedinIcon } from '../assets/SVGs/linkedin.svg'
 import { ReactComponent as Pin } from '../assets/pin.svg'
 
-
+let year = new Date().getFullYear()
 
 
 function Footer(props) {
     return (
         <div className = "footer">
             <div className = "socialIcons">
-            <a href = "https://github.com/NealGarcia" target="_blank" rel="noreferrer"><GithubIcon className = "icon" /></a>
+            <a href = "https://github.com/neal-garcia95" target="_blank" rel="noreferrer"><GithubIcon className = "icon" /></a>
             <a href = "https://www.linkedin.com/in/nealgarcia" target="_blank" rel="noreferrer"><LinkedinIcon className = "icon"/></a>
             <a href="https://www.instagram.com/neal.garcia/"><IgIcon className = "icon"/></a>
             </div>
-            Neal Garcia ©2022
+            Thanks for checkin' out my site ♥ | Neal Garcia ©{year}
         </div>
     );
 }
