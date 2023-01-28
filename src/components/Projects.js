a959af429c22488112e5ca2cc3ad55610eb3c38d
neal-garcia95 <neal@builtbymasonry.com>
2023-01-27 20:45:15 -0600

    redo projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,4 @@
 import React from 'react';
-//styles
-import "../styles/projects.css" 
-
-// screenshots
 import screen2 from '../assets/artgallery.png'
 import screen1 from '../assets/spaceman.png'
 import screen3 from '../assets/employeefinder.png'
@@ -13,8 +9,8 @@ import screen6 from '../assets/nba-teams.png'
 const projects = [
     {
         id: 1,
-        name: "travlr",
-        description:"A full-stack application where users can create, update, view, and delete records and entries of their travels.",
+        name: "Chuy's",
+        description:"In 2022 the Chuy's website went through a complete overhaul. The new website moved from ExpressionEngine to Craft CMS for client-side content management, as well as a responsive and modern makeover. I worked alongside a Lead Developer on the front-end development using Vue and Sass, utilized the Twig templating engine to connect the front-end to the CMS backend, and provided post-launch support and maintenance.",
         tags: "Front-End, Back-End, Django, Python, PostgresQL, ReactJS, React Router, CSS",
         img: screen4,
         repo: "https://github.com/NealGarcia/travel-tracker-frontend",
@@ -72,63 +68,23 @@ const projects = [
 
 function Projects(props) {
     return (
-        <div className = "projects" id="work">
-
-            <div className="project-1">
-                <div className="projectCard">
-                    <div className="content">
-                        <div className="top">
-                            <p>MASONRY</p>
-                            <p className="right">WEBSITE DEVELOPMENT & MAINTAINENCE</p>
-                        </div>       
-                        <div className="center">
-                            <p className="cardTitle">CHUY'S</p>    
-                        </div>               
-                        <div className="bottom">
-                            <p>CRAFT CMS, HTML, SASS, JAVASCRIPT, VUE</p>
-                            <a className="right" href="https://www.chuys.com" target="_blank">VIEW SITE</a>
-                        </div> 
+        <div className = "projects">
+            <div className = "projectsContainer">
+                {projects.map(project => (
+                    <div key = {project.id} className = "projectCard">
+                        <a href = {project.site} target="_blank" rel="noreferrer" className = "imglink"><img src = {project.img} alt = "Project" /></a>
+                        <div className = "projectTextWrapper">
+                            <h2 className = "projectTitle">{project.name}</h2>
+                            <p className = "projectTags">{project.tags}</p>
+                            <p className = "projectDescription">{project.description}</p>
+                            <div className = "projectLinks">
+                                <p><a href = {project.repo} target="_blank" rel="noreferrer">Source Code</a> →</p>
+                                <p><a href = {project.site} target="_blank" rel="noreferrer">Live Project </a>→</p>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <p className="projectDescription">In 2022 the Chuy's website went through a complete overhaul. The new website moved from ExpressionEngine to Craft CMS for client-side content management, as well as a responsive and modern makeover. I worked alongside a Lead Developer on the front-end development using Vue and Sass, utilized the Twig templating engine to connect the front-end to the CMS backend, and provided post-launch support and maintenance.   </p>
-            </div>
+                ))}
 
-            <div className="project-2">
-                <div className="projectCard">
-                    <div className="content">
-                        <div className="top">
-                            <p>PERSONAL PROJECT</p>
-                            <p className="right">WEBSITE DEVELOPMENT & DESIGN</p>
-                        </div>       
-                        <div className="center">
-                            <p className="cardTitle">REACT ART GALLERY</p>    
-                        </div>               
-                        <div className="bottom">
-                            <p>HTML, CSS, JAVASCRIPT, REACT, API</p>
-                            <a className="right" href="https://www.chuys.com" target="_blank">VIEW SITE</a>
-                        </div> 
-                    </div>
-                </div>
-                <p className="projectDescription">This was a project developed during my time in General Assembly's Software Engineering Immersive Program. React Art Gallery is a front-end web application that utilizes third-party data from the Art Institute of Chicago API to allow users to search and browse their collections.</p>
-            </div>
-
-            <div className="project-3">
-                <div className="projectCard">
-                    <div className="content">
-                        <div className="top">
-                            <p>PERSONAL PROJECT</p>
-                            <p className="right">WEBSITE DEVELOPMENT & DESIGN</p>
-                        </div>       
-                        <div className="center">
-                            <p className="cardTitle"></p>    
-                        </div>               
-                        <div className="bottom">
-                            <p>HTML, CSS, JAVASCRIPT, REACT, API</p>
-                            <a className="right" href="https://www.chuys.com" target="_blank">VIEW SITE</a>
-                        </div> 
-                    </div>
-                </div>
-                <p className="projectDescription">In 2022 the Chuy's website went through a complete overhaul. The new website moved from ExpressionEngine to Craft CMS for client-side content management, as well as a responsive and modern makeover. I worked alongside a Lead Developer on the front-end development using Vue and Sass, and utilized the Twig templating engine to connect the front end to the CMS backend.  </p>
             </div>
         </div>
     );
